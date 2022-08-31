75ddc2ed2b05ae53aeb1eff67145b91c39df1eb1
Ata Emir Kaba <ataemirkaba@icloud.com>
2022-08-31 23:18:57 +0300

    Initial Commit

---
+++ swiftuilearn/swiftuilearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuilearnApp.swift
+//  swiftuilearn
+//
+//  Created by Ata emir on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftuilearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
