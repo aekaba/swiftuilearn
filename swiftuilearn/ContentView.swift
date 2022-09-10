cae98d04506d156f46a9b65a633444204499a50e
Ata Emir Kaba <ataemirkaba@icloud.com>
2022-09-10 14:17:00 +0300

    State Manegement 101

--- swiftuilearn/ContentView.swift
+++ swiftuilearn/ContentView.swift
@@ -7,71 +7,28 @@
 
 import SwiftUI
 
+struct IconFieldView: View {
+    
+    @State var textFieldValue: String = ""
+    
+    var body: some View{
+        VStack {
+            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: $textFieldValue)
+            Text(textFieldValue).font(.largeTitle)
+            Image(systemName: textFieldValue.lowercased()).frame(width: 150, height: 150, alignment: Alignment.leading)
+        }
+    }
+}
+
 struct ContentView: View {
-    fileprivate func VerticalView() -> some View {
-        return VStack{
-            Spacer()
-            Text("1")
-            Spacer()
-            Text("2")
-            Spacer()
-        }
-    }
-    fileprivate func HorizontalView() -> some View {
-        return HStack{
-            Spacer()
-            Text("1")
-            Spacer()
-            Text("2")
-            Spacer()
-        }
-    }
-    fileprivate func StackView() -> some View {
-        return ZStack{
-            Spacer()
-            Color.pink
-            Spacer()
-            Text("2")
-            Spacer()
-        }
-    }
-    
-    fileprivate func FinalTestView() -> some View {
-        return VStack{
-            
-            Rectangle().foregroundColor(.green)
-            Rectangle().foregroundColor(.red)
-            
-            Rectangle().foregroundColor(.purple)
-            
-            HStack{
-                Rectangle().foregroundColor(.blue)
-                Rectangle().foregroundColor(.cyan)
-            }
-        }
-    }
-    
-    fileprivate func Groups() -> some View {
-        return VStack {
-            Group {
-                Image(systemName: "moon")
-                    .foregroundColor(.black)
-                Text("Ata Ben")
-                    .font(.headline)
-            }
-        }
-    }
-    
     var body: some View {
         Color.red
     }
 }
 
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        IconFieldView()
             .previewInterfaceOrientation(.portrait)
     }
 }
