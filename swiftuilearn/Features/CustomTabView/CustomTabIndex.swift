6d86d61a48037039ccf5ec5a493e7f9581356d63
Ata Emir Kaba <ataemirkaba@icloud.com>
2022-09-13 20:43:23 +0300

    Tabbar samples

---
+++ swiftuilearn/Features/CustomTabView/CustomTabIndex.swift
@@ -0,0 +1,13 @@
+//
+//  CustomTabIndex.swift
+//  swiftuilearn
+//
+//  Created by Ata emir on 13.09.2022.
+//
+
+import Foundation
+
+enum CustomTabs: Int {
+    case heart = 0
+    case moon = 1
+}
